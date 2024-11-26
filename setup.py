dfaf6380ac6310807235c7116d719a4101990014
Alex Rowley <arowley@gmail.com>
2024-11-26 16:49:15 +1100

    {bug} Removing history from package build

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+# with open('HISTORY.rst') as history_file:
+#     history = history_file.read()
 
 requirements = [ ]
 
@@ -31,7 +31,7 @@ setup(
     description="Creates svg based charts in python",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='pysvgchart',
     name='pysvgchart',
