3e9d1e7377c5a7f8b7a45765b1eda9ee23cdd260
Alex Rowley <arowley@gmail.com>
2025-02-28 17:11:16 +1100

    {feat} Simplify chart base class

--- pysvgchart/pysvgchart.py
+++ pysvgchart/pysvgchart.py
@@ -495,17 +495,14 @@ class Chart:
     overall svg template for chart
     """
     svg_begin_template = '<svg viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">'
-    default_major_grid_styles = {'stroke': '#2e2e2c'}
-    default_minor_grid_styles = {'stroke': '#2e2e2c', 'stroke-width': "0.4"}
 
     def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.y_axis = None
-        self.x_axis = None
-        self.legend = None
-        self.series = []
         self.custom_elements = []
+
+    def get_element_list(self):
+        raise NotImplementedError("Not implemented in generic chart.")
 
     def add_custom_element(self, custom_element):
         self.custom_elements.append(custom_element)
@@ -517,67 +514,6 @@ class Chart:
             '</svg>'
         ])
 
-    def get_element_list(self):
-        target_names = ['x_axis', 'y_axis', 'legend', 'sec_y_axis']
-        targets = [getattr(self, target) for target in target_names if getattr(self, target) is not None]
-        targets.extend(self.series[s] for s in self.series)
-        targets.extend(self.custom_elements)
-        return [e for t in targets for e in t.get_element_list()]
-
-    def add_grids(self, minor_x_ticks=0, minor_y_ticks=0, major_grid_style=None, minor_grid_style=None):
-        self.add_y_grid(minor_y_ticks, major_grid_style, minor_grid_style)
-        self.add_x_grid(minor_x_ticks, major_grid_style, minor_grid_style)
-
-    def add_y_grid(self, minor_ticks=0, major_grid_style=None, minor_grid_style=None):
-        major_style = major_grid_style.copy() if major_grid_style is not None else self.default_major_grid_styles.copy()
-        minor_style = minor_grid_style.copy() if minor_grid_style is not None else self.default_minor_grid_styles.copy()
-        for i, m in enumerate(self.x_axis.limits[1:]):
-            width_offset = (i + 1) * self.x_axis.length / (len(self.x_axis.limits) - 1) + self.y_axis.position.x
-            self.y_axis.grid_lines.append(
-                Line(
-                    x_position=width_offset,
-                    y_position=self.x_axis.position.y - self.y_axis.length,
-                    width=0,
-                    height=self.y_axis.length,
-                    styles=major_style
-                )
-            )
-            minor_step = self.x_axis.length / (len(self.x_axis.limits) - 1) / (minor_ticks + 1)
-            for j in range(1, minor_ticks + 1):
-                minor_offset = width_offset - j * minor_step
-                self.y_axis.grid_lines.append(Line(
-                    x_position=minor_offset,
-                    y_position=self.x_axis.position.y - self.y_axis.length,
-                    width=0,
-                    height=self.y_axis.length,
-                    styles=minor_style
-                ))
-
-    def add_x_grid(self, minor_ticks=0, major_grid_style=None, minor_grid_style=None):
-        major_style = major_grid_style.copy() if major_grid_style is not None else self.default_major_grid_styles.copy()
-        minor_style = minor_grid_style.copy() if minor_grid_style is not None else self.default_minor_grid_styles.copy()
-        for i, m in enumerate(self.y_axis.limits[1:]):
-            height_offset = (len(self.y_axis.limits) - 2 - i) * self.y_axis.length / (len(self.y_axis.limits) - 1) + self.x_axis.position.y
-            self.x_axis.grid_lines.append(
-                Line(
-                    x_position=self.y_axis.position.x,
-                    y_position=height_offset - self.y_axis.length,
-                    width=self.x_axis.length,
-                    height=0,
-                    styles=major_style
-                )
-            )
-            minor_step = self.y_axis.length / (len(self.y_axis.limits) - 1) / (minor_ticks + 1)
-            for j in range(1, minor_ticks + 1):
-                minor_offset = height_offset + j * minor_step
-                self.y_axis.grid_lines.append(Line(
-                    x_position=self.y_axis.position.x,
-                    y_position=minor_offset - self.y_axis.length,
-                    width=self.x_axis.length,
-                    height=0,
-                    styles=minor_style
-                ))
-
 
 class SimpleLineChart(Chart):
     """
@@ -586,6 +522,9 @@ class SimpleLineChart(Chart):
     - y values differ per line
     """
     __line_colour_defaults__ = ['green', 'red', 'blue', 'orange', 'yellow', 'black']
+
+    default_major_grid_styles = {'stroke': '#2e2e2c'}
+    default_minor_grid_styles = {'stroke': '#2e2e2c', 'stroke-width': "0.4"}
 
     def __init__(
             self,
@@ -647,6 +586,11 @@ class SimpleLineChart(Chart):
         :param width: optional width of the graph
         """
         super().__init__(height, width)
+        self.x_axis = None
+        self.y_axis = None
+        self.legend = None
+        self.sec_y_axis = None
+        self.series = []
         series_names = y_names if y_names is not None else ['Series {0}'.format(k) for k in range(len(y_values))]
         all_y_values = [v for series in y_values for v in series]
         self.y_axis = YAxis(
@@ -711,8 +655,65 @@ class SimpleLineChart(Chart):
         for index, series in enumerate(self.series):
             self.series[series].styles['stroke'] = self.__line_colour_defaults__[index % len(self.__line_colour_defaults__)]
 
+    def get_element_list(self):
+        return collapse_element_list([self.x_axis], [self.y_axis], [self.legend], [self.sec_y_axis], [self.series[s] for s in self.series], self.custom_elements)
+
     def add_legend(self, x_position=500, y_position=60, element_x=100, element_y=0, line_length=20, line_text_gap=5):
         self.legend = LineLegend(x_position, y_position, self.series, element_x, element_y, line_length, line_text_gap)
+
+    def add_grids(self, minor_x_ticks=0, minor_y_ticks=0, major_grid_style=None, minor_grid_style=None):
+        self.add_y_grid(minor_y_ticks, major_grid_style, minor_grid_style)
+        self.add_x_grid(minor_x_ticks, major_grid_style, minor_grid_style)
+
+    def add_y_grid(self, minor_ticks=0, major_grid_style=None, minor_grid_style=None):
+        major_style = major_grid_style.copy() if major_grid_style is not None else self.default_major_grid_styles.copy()
+        minor_style = minor_grid_style.copy() if minor_grid_style is not None else self.default_minor_grid_styles.copy()
+        for i, m in enumerate(self.x_axis.limits[1:]):
+            width_offset = (i + 1) * self.x_axis.length / (len(self.x_axis.limits) - 1) + self.y_axis.position.x
+            self.y_axis.grid_lines.append(
+                Line(
+                    x_position=width_offset,
+                    y_position=self.x_axis.position.y - self.y_axis.length,
+                    width=0,
+                    height=self.y_axis.length,
+                    styles=major_style
+                )
+            )
+            minor_step = self.x_axis.length / (len(self.x_axis.limits) - 1) / (minor_ticks + 1)
+            for j in range(1, minor_ticks + 1):
+                minor_offset = width_offset - j * minor_step
+                self.y_axis.grid_lines.append(Line(
+                    x_position=minor_offset,
+                    y_position=self.x_axis.position.y - self.y_axis.length,
+                    width=0,
+                    height=self.y_axis.length,
+                    styles=minor_style
+                ))
+
+    def add_x_grid(self, minor_ticks=0, major_grid_style=None, minor_grid_style=None):
+        major_style = major_grid_style.copy() if major_grid_style is not None else self.default_major_grid_styles.copy()
+        minor_style = minor_grid_style.copy() if minor_grid_style is not None else self.default_minor_grid_styles.copy()
+        for i, m in enumerate(self.y_axis.limits[1:]):
+            height_offset = (len(self.y_axis.limits) - 2 - i) * self.y_axis.length / (len(self.y_axis.limits) - 1) + self.x_axis.position.y
+            self.x_axis.grid_lines.append(
+                Line(
+                    x_position=self.y_axis.position.x,
+                    y_position=height_offset - self.y_axis.length,
+                    width=self.x_axis.length,
+                    height=0,
+                    styles=major_style
+                )
+            )
+            minor_step = self.y_axis.length / (len(self.y_axis.limits) - 1) / (minor_ticks + 1)
+            for j in range(1, minor_ticks + 1):
+                minor_offset = height_offset + j * minor_step
+                self.y_axis.grid_lines.append(Line(
+                    x_position=self.y_axis.position.x,
+                    y_position=minor_offset - self.y_axis.length,
+                    width=self.x_axis.length,
+                    height=0,
+                    styles=minor_style
+                ))
 
 
 class DonutChart(Chart):
@@ -743,4 +744,4 @@ class DonutChart(Chart):
             start_theta = end_theta
 
     def get_element_list(self):
-        return collapse_element_list(self.segments)
+        return collapse_element_list(self.segments, self.custom_elements)
