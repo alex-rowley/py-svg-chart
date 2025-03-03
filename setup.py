acbb1ed31ad62cb75a41e08f547008d2c0d5ec58
Alex Rowley <arowley@gmail.com>
2025-03-03 22:29:42 +1100

    chore: Release 0.0.12

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/arowley-ai/py-svg-chart',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
