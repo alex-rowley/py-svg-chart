f88be86fed260dbc2ebc8f7031613ae79553cc0d
Alex Rowley <arowley@gmail.com>
2025-05-16 15:46:22 +1000

    chore: Release 0.3.2

--- pysvgchart/__init__.py
+++ pysvgchart/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Alex Rowley'
 __email__ = ''
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from .charts import LineChart, SimpleLineChart, DonutChart, BarChart, NormalisedBarChart, ScatterChart
 from .shapes import Text, Line, Circle
